1842e3cc98db7097cd8771309f217c080f4a2416
kalsariyadarshan87 <159775520+kalsariyadarshan87@users.noreply.github.com>
2024-02-13 15:52:38 +0530

    Add files via upload

---
+++ t1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main(){
+	printf("kalsariya darshan d\n");
+	printf("age:20\n");
+	printf("school:the school of science");
+	
+}
---
+++ t2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main(){
+	printf("--------\n");
+	printf("|         |\n");
+	printf("d         |\n");
+	printf("a         |\n");
+	printf("r         |\n");
+	printf("s         |\n");
+	printf("---------\n");
+}
